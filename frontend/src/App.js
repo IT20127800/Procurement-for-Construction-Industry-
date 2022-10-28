d8b035821e06224285ee6fd2463de9d8c8f7c37b
pamitha <pamitha.creativehub@gmail.com>
2022-10-28 14:00:09 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, withRouter } from "react-router-dom";
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+      <div>
+        <h1>Hallo</h1>
+      </div>
+  
+    </BrowserRouter>
+  );
+}
+
+export default App;
